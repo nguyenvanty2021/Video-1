21bb789343914aca8d21214618c2edc2c9f69cf4
Tỷ Nguyễn <tynguyen@Tys-MacBook-Pro.local>
2022-03-14 15:03:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import VideoPlayer from 'react-video-js-player'
 import './App.css';
-
+import Car from './video/car.mp4';
 function App() {
+  const videoSrc = Car;
+  const poster = 'https://betraining.org/wp-content/uploads/2013/10/return-of-video-to-elearning.jpg';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VideoPlayer src={videoSrc} playbackRates={[0.5, 1, 2, 3, 4, 5]} poster={poster} width={720} height={420} />
+      <video autoPlay loop muted style={{}} >
+        <source src={Car} type='video/mp4' />
+      </video>
     </div>
   );
 }
